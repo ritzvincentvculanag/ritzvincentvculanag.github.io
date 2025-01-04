bcf94b3d2c2c2a2620cfdfb3153dbd0d6485e93a
ritzvincentculanag <ritzvincentvergaraculanag@gmail.com>
2025-01-04 22:41:56 +0800

    add a listener to automatically reset navbar when resized to desktop

--- scripts/index.js
+++ scripts/index.js
@@ -7,8 +7,18 @@ let isToggled = false;
 navMainTrigger.addEventListener("click", triggerNavBar);
 navMainReturn.addEventListener("click", triggerNavBar);
 
+window.addEventListener("resize", resetNavBar);
+
 function triggerNavBar() {
   isToggled = !isToggled;
   const rightValue = isToggled ? "0px" : "-400px";
   navMain.style.right = rightValue;
+}
+
+function resetNavBar() {
+  if (window.innerHeight >= 720) {
+    navMain.style.right = "0px";
+    isToggled = false;
+    triggerNavBar();
+  }
 }
