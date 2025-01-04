57e014739ff690547028d25badbb40a25bffef37
ritzvincentculanag <ritzvincentvergaraculanag@gmail.com>
2025-01-04 23:45:57 +0800

    fix issues of reseting navbar

--- scripts/index.js
+++ scripts/index.js
@@ -10,15 +10,19 @@ navMainReturn.addEventListener("click", triggerNavBar);
 window.addEventListener("resize", resetNavBar);
 
 function triggerNavBar() {
-  isToggled = !isToggled;
-  const rightValue = isToggled ? "0px" : "-400px";
+  if (window.innerWidth <= 375 && isToggled) {
+    isToggled = true;
+    navMain.style.right = "-400px";
+    return;
+  }
+
+  const rightValue = isToggled ? "-400px" : "0px";
   navMain.style.right = rightValue;
+  isToggled = !isToggled;
 }
 
 function resetNavBar() {
-  if (window.innerHeight >= 720) {
-    navMain.style.right = "0px";
-    isToggled = false;
-    triggerNavBar();
-  }
+  isToggled = false;
+  const rightValue = window.innerWidth >= 720 ? "0px" : "-400px";
+  navMain.style.right = rightValue;
 }
